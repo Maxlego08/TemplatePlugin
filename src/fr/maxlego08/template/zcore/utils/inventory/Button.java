def58ce4d4a89929646e8c1773e831c984b1f671
Maxlego08 <maxlego08@sfr.fr>
2020-03-20 14:44:48 +0100

    Ajout du système de loader

--- src/fr/maxlego08/template/zcore/utils/inventory/Button.java
+++ src/fr/maxlego08/template/zcore/utils/inventory/Button.java
@@ -1,6 +1,5 @@
 package fr.maxlego08.template.zcore.utils.inventory;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.inventory.ItemStack;
@@ -10,19 +9,32 @@ import fr.maxlego08.template.zcore.utils.ZUtils;
 
 public class Button extends ZUtils {
 
-	private String name;
-	private int item;
-	private List<String> lore;
+	private final String name;
+	private final int item;
+	private final int data;
+	private final List<String> lore;
+	private int category;
 
-	public Button(String name, int item, List<String> lore) {
+
+	public Button(String name, int item, int data, List<String> lore) {
 		this.name = name;
 		this.lore = lore;
 		this.item = item;
+		this.data = data;
 	}
 
-	public Button(String name, int item) {
+	public Button(String name, int item, int data) {
 		this.name = name;
 		this.item = item;
+		this.data = data;
+		this.lore = null;
+	}
+
+	public Button(String name, int item, int data, int category) {
+		this.name = name;
+		this.item = item;
+		this.category = category;
+		this.data = data;
 		this.lore = null;
 	}
 
@@ -34,42 +46,73 @@ public class Button extends ZUtils {
 		return lore;
 	}
 
+	public int getData() {
+		return data;
+	}
+
+	public int getItemInInteger() {
+		return item;
+	}
+
+	/**
+	 * @return the category
+	 */
+	public int getCategory() {
+		return category;
+	}
+
+	/**
+	 * @param category
+	 *            the category to set
+	 */
+	public void setCategory(int category) {
+		this.category = category;
+	}
+
+	@SuppressWarnings("deprecation")
 	public ItemStack getItem() {
-		return new ItemStack(getMaterial(item));
+		return new ItemStack(getMaterial(item), 1, (byte) data);
 	}
 
 	public boolean hasLore() {
 		return lore != null;
 	}
 
+	public boolean hasName() {
+		return name != null;
+	}
+
 	public ItemStack getInitButton() {
 		ItemStack item = getItem();
 		ItemMeta itemM = item.getItemMeta();
-		itemM.setDisplayName(getName());
-		if (hasLore()) {
+		if (hasName())
+			itemM.setDisplayName(getName());
+		if (hasLore())
 			itemM.setLore(getLore());
-		}
 		item.setItemMeta(itemM);
 		return item;
 	}
 
-	public ItemStack getInitButton(String replaceItem, String replacer) {
-		return getInitButton(replaceItem, replacer, 1);
-	}
-
-	public ItemStack getInitButton(String replaceItem, String replacer, int how) {
+	public ItemStack getInitButton(String type, String replace) {
 		ItemStack item = getItem();
 		ItemMeta itemM = item.getItemMeta();
-		itemM.setDisplayName(getName());
-		if (hasLore()) {
-			List<String> list = new ArrayList<>();
-			for (String d : getLore())
-				list.add(d.replace(replacer, replaceItem));
-			itemM.setLore(list);
-		}
-		item.setAmount(how);
+		if (hasName())
+			itemM.setDisplayName(getName().replace(type, replace));
+		if (hasLore())
+			itemM.setLore(getLore());
 		item.setItemMeta(itemM);
 		return item;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Button [name=" + name + ", item=" + item + ", data=" + data + ", lore=" + lore + ", category="
+				+ category + "]";
 	}
 
 }
