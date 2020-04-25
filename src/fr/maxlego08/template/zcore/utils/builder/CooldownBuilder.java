ab7fa11c146a62cec94fd38839538f4569479997
Maxlego08 <maxlego08@sfr.fr>
2020-04-25 15:35:53 +0200

    Rework CooldownBuilder

--- src/fr/maxlego08/template/zcore/utils/builder/CooldownBuilder.java
+++ src/fr/maxlego08/template/zcore/utils/builder/CooldownBuilder.java
@@ -1,6 +1,7 @@
 package fr.maxlego08.template.zcore.utils.builder;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import org.bukkit.entity.Player;
@@ -8,55 +9,137 @@ import org.bukkit.entity.Player;
 import fr.maxlego08.template.zcore.utils.storage.Persist;
 import fr.maxlego08.template.zcore.utils.storage.Saveable;
 
-public class CooldownBuilder implements Saveable{
+public class CooldownBuilder implements Saveable {
 
-	public static HashMap<String, HashMap<UUID, Long>> cooldowns = new HashMap<>();
+	public static Map<String, Map<UUID, Long>> cooldowns = new HashMap<>();
 
-	public static HashMap<UUID, Long> getCooldownMap(String s) {
-		if (cooldowns.containsKey(s)) {
-			return cooldowns.get(s);
-		}
-		return null;
+	/**
+	 * 
+	 * @param
+	 * @return
+	 */
+	public static Map<UUID, Long> getCooldownMap(String key) {
+		return cooldowns.getOrDefault(key, null);
 	}
 
+	/**
+	 * 
+	 */
 	public static void clear() {
 		cooldowns.clear();
 	}
 
-	public static void createCooldown(String s) {
-		if (!cooldowns.containsKey(s)) {
-			cooldowns.put(s, new HashMap<>());
-		}
+	/**
+	 * 
+	 * @param key
+	 */
+	public static void createCooldown(String key) {
+		cooldowns.putIfAbsent(key, new HashMap<>());
 	}
 
-	public static void removeCooldown(String s, Player joueur) {
-		if (!cooldowns.containsKey(s)) {
-			throw new IllegalArgumentException("! Attention ! " + String.valueOf(s) + " n'existe pas.");
-		}
-		(cooldowns.get(s)).remove(joueur.getUniqueId());
+	/**
+	 * 
+	 * @param key
+	 * @param joueur
+	 */
+	public static void removeCooldown(String key, UUID uuid) {
+
+		createCooldown(key);
+
+		getCooldownMap(key).remove(uuid);
 	}
 
-	public static void addCooldown(String s, Player joueur, int seconds){
-		if (!cooldowns.containsKey(s)) {
-			throw new IllegalArgumentException(String.valueOf(s) + " n'existe pas.");
-		}
+	/**
+	 * 
+	 * @param key
+	 * @param player
+	 */
+	public static void removeCooldown(String key, Player player) {
+		removeCooldown(key, player.getUniqueId());
+	}
+
+	/**
+	 * 
+	 * @param key
+	 * @param joueur
+	 * @param seconds
+	 */
+	public static void addCooldown(String key, UUID uuid, int seconds) {
+
+		createCooldown(key);
+
 		long next = System.currentTimeMillis() + seconds * 1000L;
-		(cooldowns.get(s)).put(joueur.getUniqueId(), Long.valueOf(next));
-	}
-
-	public static boolean isCooldown(String s, Player joueur) {
-		return (cooldowns.containsKey(s)) && ((cooldowns.get(s)).containsKey(joueur.getUniqueId()))
-				&& (System.currentTimeMillis() <= ((Long) (cooldowns.get(s)).get(joueur.getUniqueId())).longValue());
-	}
-
-	public static long getCooldownPlayer(String s, Player joueur) {
-		return ((Long) (cooldowns.get(s)).getOrDefault(joueur.getUniqueId(), 0l)).longValue() - System.currentTimeMillis();
-	}
-
-	public static String getCooldownAsString(String s, Player player) {
-		return TimerBuilder.getStringTime(getCooldownPlayer(s, player) / 1000);
+		getCooldownMap(key).put(uuid, Long.valueOf(next));
 	}
 	
+	/**
+	 * 
+	 * @param key
+	 * @param player
+	 * @param seconds
+	 */
+	public static void addCooldown(String key, Player player, int seconds) {
+		addCooldown(key, player.getUniqueId(), seconds);
+	}
+
+	/**
+	 * 
+	 * @param key
+	 * @param uuid
+	 * @return boolean
+	 */
+	public static boolean isCooldown(String key, UUID uuid) {
+
+		createCooldown(key);
+		Map<UUID, Long> map = new HashMap<>();
+
+		return (map.containsKey(uuid)) && (System.currentTimeMillis() <= ((Long) map.get(uuid)).longValue());
+	}
+
+	/**
+	 * 
+	 * @param key
+	 * @param player
+	 * @return boolean
+	 */
+	public static boolean isCooldown(String key, Player player) {
+		return isCooldown(key, player.getUniqueId());
+	}
+
+	/**
+	 * 
+	 * @param key
+	 * @param uuid
+	 * @return long
+	 */
+	public static long getCooldown(String key, UUID uuid) {
+
+		createCooldown(key);
+		Map<UUID, Long> map = new HashMap<>();
+
+		return ((Long) map.getOrDefault(uuid, 0l)).longValue() - System.currentTimeMillis();
+	}
+
+	/**
+	 * 
+	 * @param key
+	 * @param player
+	 * @return long
+	 */
+	public static long getCooldownPlayer(String key, Player player) {
+		return getCooldown(key, player.getUniqueId());
+	}
+
+	/**
+	 * 
+	 * @param key
+	 * @param player
+	 * @return
+	 */
+	public static String getCooldownAsString(String key, UUID player) {
+		return TimerBuilder.getStringTime(getCooldown(key, player) / 1000);
+	}
+
 	private static transient CooldownBuilder i = new CooldownBuilder();
 
 	@Override
