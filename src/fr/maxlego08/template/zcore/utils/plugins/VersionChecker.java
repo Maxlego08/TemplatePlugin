9d840e55d9e7b59582e8942f5ba2b8528549d865
Maxlego08 <maxlego08@sfr.fr>
2021-05-11 12:44:55 +0200

    :construction: Update version checker

--- src/fr/maxlego08/template/zcore/utils/plugins/VersionChecker.java
+++ src/fr/maxlego08/template/zcore/utils/plugins/VersionChecker.java
@@ -1,94 +1,113 @@
 package fr.maxlego08.template.zcore.utils.plugins;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import fr.maxlego08.template.listener.ListenerAdapter;
+import fr.maxlego08.template.zcore.enums.Message;
 import fr.maxlego08.template.zcore.logger.Logger;
 
-public class VersionChecker extends ListenerAdapter {
+/**
+ * 
+ * @author Maxlego08
+ *
+ */
+public class VersionChecker implements Listener {
 
+	private final String URL_API = "https://groupez.dev/api/v1/resource/version/%s";
+	private final String URL_RESOURCE = "https://groupez.dev/resources/%s";
 	private final Plugin plugin;
-
-	/**
-	 * @param plugin
-	 * @param useLastVersion
-	 */
-	public VersionChecker(Plugin plugin) {
-		super();
-		this.plugin = plugin;
-	}
-
+	private final int pluginID;
 	private boolean useLastVersion = false;
 
-	public void useLastVersion(Plugin plugin) {
-		UpdateChecker checker = new UpdateChecker(plugin, 74073);
+	/**
+	 * Class constructor
+	 * 
+	 * @param plugin
+	 * @param pluginID
+	 */
+	public VersionChecker(Plugin plugin, int pluginID) {
+		super();
+		this.plugin = plugin;
+		this.pluginID = pluginID;
+	}
+
+	/**
+	 * Allows to check if the plugin version is up to date.
+	 */
+	public void useLastVersion() {
+
+		Bukkit.getPluginManager().registerEvents(this, this.plugin); // Register
+																		// event
+
+		String pluginVersion = plugin.getDescription().getVersion();
 		AtomicBoolean atomicBoolean = new AtomicBoolean();
-		checker.getVersion(version -> {
+		this.getVersion(version -> {
 
 			long ver = Long.valueOf(version.replace(".", ""));
-			long plVersion = Long.valueOf(plugin.getDescription().getVersion().replace(".", ""));
+			long plVersion = Long.valueOf(pluginVersion.replace(".", ""));
 			atomicBoolean.set(plVersion >= ver);
-			useLastVersion = atomicBoolean.get();
+			this.useLastVersion = atomicBoolean.get();
 			if (atomicBoolean.get())
-				Logger.info("There is not a new update available.");
-			else
-				Logger.info("There is a new update available. Your version: " + plugin.getDescription().getVersion()
-						+ ", Laste version: " + version);
+				Logger.info("No update available.");
+			else {
+				Logger.info("New update available. Your version: " + pluginVersion + ", latest version: " + version);
+				Logger.info("Download plugin here: " + String.format(URL_RESOURCE, this.pluginID));
+			}
 		});
 
 	}
 
-	@Override
-	protected void onConnect(PlayerJoinEvent event, Player player) {
-		new BukkitRunnable() {
-			@Override
-			public void run() {
-				if (!useLastVersion && event.getPlayer().hasPermission("zplugin.notifs")) {
-					message(player,
-							"§cYou do not use the latest version of the plugin! Thank you for taking the latest version to avoid any risk of problem!");
+	@EventHandler
+	public void onConnect(PlayerJoinEvent event) {
+		final Player player = event.getPlayer();
+		if (!useLastVersion && event.getPlayer().hasPermission("zplugin.notifs")) {
+			new BukkitRunnable() {
+				@Override
+				public void run() {
+					String prefix = Message.PREFIX.getMessage();
+					player.sendMessage(prefix
+							+ "§cYou do not use the latest version of the plugin! Thank you for taking the latest version to avoid any risk of problem!");
+					player.sendMessage(prefix + "§fDownload plugin here: §a" + String.format(URL_RESOURCE, pluginID));
 				}
-			}
-		}.runTaskLater(plugin, 20 * 2);
+			}.runTaskLater(plugin, 20 * 2);
+		}
 	}
 
-	public class UpdateChecker {
+	/**
+	 * Get version by plugin id
+	 * 
+	 * @param consumer
+	 *            - Do something after
+	 */
+	public void getVersion(Consumer<String> consumer) {
+		Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
+			final String apiURL = String.format(URL_API, this.pluginID);
+			try {
+				URL url = new URL(apiURL);
+				URLConnection hc = url.openConnection();
+				hc.setRequestProperty("User-Agent",
+						"Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
+				Scanner scanner = new Scanner(hc.getInputStream());
+				if (scanner.hasNext())
+					consumer.accept(scanner.next());
+				scanner.close();
 
-		private Plugin plugin;
-		private int resourceId;
-
-		public UpdateChecker(Plugin plugin, int resourceId) {
-			this.plugin = plugin;
-			this.resourceId = resourceId;
-		}
-
-		/**
-		 * 
-		 * @param consumer
-		 */
-		public void getVersion(final Consumer<String> consumer) {
-			Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
-				try (InputStream inputStream = new URL(
-						"https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId).openStream();
-						Scanner scanner = new Scanner(inputStream)) {
-					if (scanner.hasNext()) {
-						consumer.accept(scanner.next());
-					}
-				} catch (IOException exception) {
-					this.plugin.getLogger().info("Cannot look for updates: " + exception.getMessage());
-				}
-			});
-		}
+			} catch (IOException exception) {
+				this.plugin.getLogger().info("Cannot look for updates: " + exception.getMessage());
+			}
+		});
 	}
 
 }
