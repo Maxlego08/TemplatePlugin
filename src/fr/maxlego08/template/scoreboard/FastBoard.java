76bf870cb47fa6a9fca25326f431fa37f49410f5
Maxence Simon <32517160+Maxlego08@users.noreply.github.com>
2021-10-15 14:17:34 +0200

    Update FastBoard.java

--- src/fr/maxlego08/template/scoreboard/FastBoard.java
+++ src/fr/maxlego08/template/scoreboard/FastBoard.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package fr.mrmicky.fastboard;
+package fr.maxlego08.template.scoreboard;
 
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
