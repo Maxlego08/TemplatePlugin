1f7c28819dc06c25f2d144dbc5b6ebebaafc68ad
Maxence Simon <32517160+Maxlego08@users.noreply.github.com>
2021-10-15 14:17:23 +0200

    Update FastReflection.java

--- src/fr/maxlego08/template/scoreboard/FastReflection.java
+++ src/fr/maxlego08/template/scoreboard/FastReflection.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package fr.mrmicky.fastboard;
+package fr.maxlego08.template.scoreboard;
 
 import org.bukkit.Bukkit;
 
